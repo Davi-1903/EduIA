eeb2dd809b7126dab327d11417c7990a95c34001
Davi Francisco <franciscodavi327@gmail.com>
2025-12-16 10:50:16 -0300

    style: adicionar página de erro 404

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,6 +7,7 @@ import Layout from './layout';
 const SignUp = lazy(() => import('./pages/unprotected/signup'));
 const SignIn = lazy(() => import('./pages/unprotected/signin'));
 const LandingPage = lazy(() => import('./pages/unprotected/landingpage'));
+const Error404 = lazy(() => import('./pages/errors/error404'));
 
 import './globals.css';
 
@@ -21,6 +22,7 @@ const router = createBrowserRouter([
             { path: 'historico', element: <h1>Historico</h1> },
             { path: 'sobre', element: <h1>Sobre</h1> },
             { path: 'config', element: <h1>Config</h1> },
+            { path: '*', element: <Error404 /> },
         ],
     },
     { path: 'login', element: <SignIn /> },
