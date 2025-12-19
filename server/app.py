25e43341976c5c9c8ca912220a0bc46303fb6888
Cecilya <anacecilya17@gmail.com>
2025-12-19 20:54:52 -0300

    feat: criar aplicação principal Flask

--- server/app.py
+++ server/app.py
@@ -1,1 +1,26 @@
 from models import database
+from flask import Flask, send_from_directory
+from controllers.auth import bp_auth
+from flask_cors import CORS
+import os
+
+app = Flask(__name__, static_folder='../client/dist')
+app.secret_key = 'secret-key'
+CORS(app, supports_credentials=True)
+app.register_blueprint(bp_auth)
+
+# Rota "Catch-All" para o React
+@app.route('/')
+@app.route('/<path:route>')
+def serve_react(route: str = ''):
+    if app.static_folder is None:
+        raise RuntimeError('A pasta estática não foi definida')
+
+    if route and not route.startswith('api') and os.path.exists(os.path.join(app.static_folder, route)):
+        return send_from_directory(app.static_folder, route)
+
+    return send_from_directory(app.static_folder, 'index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
