328ce7604656350a2b58cf422fbf117d7c77c5c5
Davi Francisco <franciscodavi327@gmail.com>
2025-12-21 17:41:04 -0300

    feat: mudar redirecionamento quando o usuário estiver autenticado

--- client/src/pages/errors/error404/index.jsx
+++ client/src/pages/errors/error404/index.jsx
@@ -1,8 +1,11 @@
 import { Link } from 'react-router-dom';
-import Mascote from '/assets/images/mascote/error_404.png';
 import { Helmet } from 'react-helmet-async';
+import { useAuthenticated } from '../../../context/authContext';
+import Mascote from '/assets/images/mascote/error_404.png';
 
 export default function Error404() {
+    const { isAuthenticated } = useAuthenticated();
+
     return (
         <>
             <Helmet>
@@ -15,8 +18,7 @@ export default function Error404() {
                 <article className='flex-1 text-center md:text-left'>
                     <h1 className='text-color1-200 mb-2 text-4xl font-semibold md:text-6xl'>Not found Error</h1>
                     <h2 className='text-color3-100 text-xl md:text-2xl'>Página não encontrada</h2>
-                    {/* Quando estiver autenticado mudar a rota */}
-                    <Link to='/'>
+                    <Link to={isAuthenticated ? '/dash' : '/'}>
                         <button className='bg-color1-100 text-color4-200 font-primary hover:shadow-lg-hard text-md mt-6 cursor-pointer rounded-xl px-4 py-4 transition-all duration-200 md:px-8 md:text-xl'>
                             Voltar para o inicio
                         </button>
