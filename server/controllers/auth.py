731a5f4a990488529a056ac0888bb325935f6fa9
Maria <garciamariadasgracas318@gmail.com>
2025-12-21 11:30:28 -0300

    criar rota de register e redirecionar a rota de login para dash

--- server/controllers/auth.py
+++ server/controllers/auth.py
@@ -1,13 +1,43 @@
+import json
 from flask import Blueprint, jsonify, request
-from flask_login import login_user
-from argon2.exceptions import VerifyMismatchError
+from flask_login import login_user, user_accessed
 from argon2 import PasswordHasher
-from models.database import User
+from argon2.exceptions import VerifyMismatchError
+from models.database import User, Aluno, Professor
 from models.connection import SessionLocal
 
-
-bp_auth = Blueprint('api', __name__, url_prefix='/api/auth')
+bp_auth = Blueprint('auth', __name__, url_prefix='/api/auth')
 senha_hasher = PasswordHasher()
+
+@bp_auth.route('/register', methods=['POST'])
+def register():
+    with SessionLocal() as session:
+        try:
+            data = request.get_json()
+            if not data:
+                return jsonify({'ok': False, 'message': 'Dados não recebidos'}), 400
+            
+            user = session.query(User).filter_by(email=data['email']).first()
+
+            if user:
+                return jsonify({'ok': False, 'message': 'Usuário já existe'}), 409
+            
+            if data['type'] == 'Student':
+                new_user = Aluno(user_name=data['nome'], email=data['email'], password=senha_hasher.hash(data['password']))
+            elif data['type'] == 'Teacher':
+                new_user = Professor(user_name=data['nome'], email=data['email'], password=senha_hasher.hash(data['password']))
+            else: 
+                return jsonify({'ok': False, 'message': 'Tipo inválido'}), 400
+            
+            session.add(new_user)
+            session.commit()
+            login_user(new_user)
+
+            return jsonify({'ok': True, 'redirect': '/dash'}), 201
+
+        except Exception as e:
+            return jsonify({'ok': False, 'message': 'Erro interno'}), 500
+
 
 @bp_auth.route('/login', methods=['POST'])
 def login():
@@ -26,7 +56,7 @@ def login():
             senha_hasher.verify(user_exist.password, senha)
             login_user(user_exist)
 
-            return jsonify({'ok': True}), 200
+            return jsonify({'ok': True, 'redirect': '/dash'}), 200
 
         except VerifyMismatchError:
             return jsonify({'ok': False, 'message': 'Senha incorreta'}), 401
