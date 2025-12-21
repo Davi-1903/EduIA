2ddad5538794fdae8f47a57557835705137c6ccd
Maria <garciamariadasgracas318@gmail.com>
2025-12-21 11:29:20 -0300

    remover campo de full_name e mudar alguns campos para tratar no futuro

--- server/models/database.py
+++ server/models/database.py
@@ -10,7 +10,6 @@ class User(Base, UserMixin):
 
     id:Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     user_name:Mapped[str] = mapped_column(String(30), unique=True, nullable=False)
-    full_name:Mapped[str] = mapped_column(nullable=False)
     email:Mapped[str] = mapped_column(unique=True, nullable=False)
     password:Mapped[str] = mapped_column(nullable=False)
 
@@ -27,18 +26,20 @@ class Professor(User):
     __tablename__ = 'professores'
 
     id:Mapped[int] = mapped_column(ForeignKey("users.id"),primary_key=True)
-    materia:Mapped[str] = mapped_column(nullable=False)
-    turma:Mapped[str] = mapped_column(nullable=False)
+    # por enquanto, os campos abaixo vão poder aceitar valores nulos pois ainda não temos páginas para o usuário indicar esses dados
+    materia:Mapped[str] = mapped_column(nullable=True)
+    turma:Mapped[str] = mapped_column(nullable=True)
 
     __mapper_args__ = {
         "polymorphic_identity": "professor"
     }
-
 class Aluno(User):
+
     __tablename__ = 'alunos'
 
     id:Mapped[int] = mapped_column(ForeignKey("users.id"),primary_key=True)
-    turma:Mapped[str] = mapped_column(nullable=False)
+    # por enquanto, o campo abaixo vai poder aceitar valores nulos pois ainda não temos páginas para o usuário indicar esses dados
+    turma:Mapped[str] = mapped_column(nullable=True)
 
     __mapper_args__ = {
         "polymorphic_identity": "aluno"
