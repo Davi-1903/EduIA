270756f196669e54339088b6398671c17390fb9f
Maria <garciamariadasgracas318@gmail.com>
2025-12-21 11:28:43 -0300

    configurar app para o flask-login

--- server/app.py
+++ server/app.py
@@ -2,12 +2,29 @@ from models import database
 from flask import Flask, send_from_directory
 from controllers.auth import bp_auth
 from flask_cors import CORS
+from flask_login import LoginManager
+from models.database import User
+from models.connection import SessionLocal
 import os
 
 app = Flask(__name__, static_folder='../client/dist')
 app.secret_key = 'secret-key'
+
+# Flask-Login
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# CORS
 CORS(app, supports_credentials=True)
+
+# Blueprints
 app.register_blueprint(bp_auth)
+
+# user_loader
+@login_manager.user_loader
+def load_user(user_id):
+    with SessionLocal() as session:
+        return session.get(User, int(user_id))
 
 # Rota "Catch-All" para o React
 @app.route('/')
@@ -20,7 +37,6 @@ def serve_react(route: str = ''):
         return send_from_directory(app.static_folder, route)
 
     return send_from_directory(app.static_folder, 'index.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
