93c4f732cae6ae623a26ca832e14ae5d724d0ab5
Davi Francisco <franciscodavi327@gmail.com>
2025-12-23 21:34:45 -0300

    fix: resolver bug visual

--- client/src/pages/unprotected/landingpage/components/question/index.jsx
+++ client/src/pages/unprotected/landingpage/components/question/index.jsx
@@ -9,8 +9,8 @@ export default function Question({ question, answer }) {
     }
 
     return (
-        <article className='border-b-color3-400/25 overflow-hidden border-b-2'>
-            <div className='flex cursor-pointer items-center justify-between gap-4 p-4' onClick={toggleOpen}>
+        <article className='border-b-color3-400/25 mb-2 overflow-hidden border-b-2 p-4'>
+            <div className='flex cursor-pointer items-center justify-between gap-4' onClick={toggleOpen}>
                 <span className='font-primary text-color1-100 text-xl font-semibold text-balance'>{question}</span>
                 <IconPlus
                     role='button'
@@ -19,8 +19,8 @@ export default function Question({ question, answer }) {
                     className={`bg-color1-100 stroke-color4-100 box-content shrink-0 rounded-full p-1 transition-all duration-150 ${open ? 'rotate-45' : ''}`}
                 />
             </div>
-            <div className={`grid transition-all duration-150 ${open ? 'grid-rows-[1fr]' : 'grid-rows-[0fr]'}`}>
-                <div className='overflow-hidden p-4 pt-0'>
+            <div className={`grid pt-2 transition-all duration-150 ${open ? 'grid-rows-[1fr]' : 'grid-rows-[0fr]'}`}>
+                <div className='overflow-hidden pt-0'>
                     <p className='font-secundary text-color3-100 text-lg/normal'>{answer}</p>
                 </div>
             </div>
