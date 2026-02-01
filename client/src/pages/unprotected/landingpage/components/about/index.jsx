b28d88a69e04be3f9cb7341c446d31202ff81754
Davi Francisco <franciscodavi327@gmail.com>
2026-02-01 16:24:47 -0300

    style: alterar loading de autenticação e adicionar gradientes

--- client/src/pages/unprotected/landingpage/components/about/index.jsx
+++ client/src/pages/unprotected/landingpage/components/about/index.jsx
@@ -25,13 +25,13 @@ export default function LandingPageAbout() {
     return (
         <section
             ref={sectionRef}
-            className='bg-color4-200 flex flex-wrap items-center justify-center gap-8 px-8 py-32 md:min-h-screen md:p-24 md:pt-0'
+            className='from-color1-400/15 to-color4-200 flex flex-wrap items-center justify-center gap-8 bg-radial-[at_right] to-60% px-8 py-32 md:min-h-screen md:bg-radial-[at_75%] md:p-24 md:pt-0'
         >
             <article className={`max-w-200 flex-2 basis-80 opacity-0 ${isEmerge ? 'animate-emerge-left' : ''}`}>
                 <h2 className='from-color1-100 to-color4-25 font-primary mb-4 bg-linear-10 bg-clip-text text-4xl font-bold text-transparent md:text-5xl'>
                     O que é o EduIA?
                 </h2>
-                <p className='text-base text-color3-100 font-secundary text-balance md:text-xl/normal'>
+                <p className='text-color3-100 font-secundary text-base text-balance md:text-xl/normal'>
                     O <strong>EduIA</strong> é um sistema de geração de materiais com <strong>IA</strong> (Inteligência
                     Artificial), desenvolvido por estudantes do ensino médio técnico integrado do <strong>IFRN</strong>{' '}
                     - campus Caicó. O sistema visa reduzir o tempo gasto por alunos e professores, gerando materiais de
